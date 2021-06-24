3f161eaa3f0b3ef32802e856ba953454aee91a19
Julianne <juliannelicon@gmail.com>
2021-06-24 14:47:10 -0300

    Adiciona página de cadastro de perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
