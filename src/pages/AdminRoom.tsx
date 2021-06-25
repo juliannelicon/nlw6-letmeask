cfc2d3ff4bf55492ff6c05751348b69f0f2fdf0b
Julianne <juliannelicon@gmail.com>
2021-06-25 18:08:14 -0300

    Adiciona botão apagar pergunta

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,13 +2,16 @@ import { useParams } from 'react-router-dom';
 
 import { useRoom } from '../hooks/useRoom';
 
+import { database } from '../services/firebase';
+
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 
 import '../styles/room.scss';
-import { Question } from '../components/Question';
 
 type RoomParams = {
   id: string;
@@ -18,6 +21,12 @@ export function AdminRoom() {
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { title, questions } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -44,7 +53,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
